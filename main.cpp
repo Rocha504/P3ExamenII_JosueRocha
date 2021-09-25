76ee5f0df58e794c082222e8b3b69167d2f3855a
Rocha504 <josuerocha504@gmail.com>
2021-09-24 20:11:31 -0400

    Menu

--- main.cpp
+++ main.cpp
@@ -19,7 +19,12 @@ int main(int argc, char** argv) {
 	op=menu();
 	while(op!=2){
 		if(op==1){
-			 
+			vector<vector<string>> operaciones=leerArchivo("Prueba.txt");
+			for(int i=0;i<operaciones.size();i++){
+				evaluarOperacion(operaciones[i]);
+				cout<<"----------------------------------------"<<endl;
+			}
+			
 		}
 	}
 	return 0;
@@ -29,12 +34,8 @@ vector<vector<string>> leerArchivo(string nombreA){
 	
 	string linea;
 	vector<vector<string>> operaciones;
-	 
 	ifstream archivo;
-	string nombreArchivo;
-	cout<<"Cual es el nombre de el archivo que desea leer?(Porfavor incluir la extencion .txt)"<<endl;
-	getline(cin,nombreArchivo);
-	archivo.open(nombreArchivo.c_str(),ios::in);
+	archivo.open(nombreA.c_str(),ios::in);
 	while(!archivo.eof()){
 		getline(archivo,linea);
 		operaciones.push_back(tokenizer(linea));
@@ -103,46 +104,46 @@ void evaluarOperacion(vector <string> operacion){
 	else
 		if(tipo=="F"){//Tipo Float
 			Stack<float>* pila = new Stack<float>();
-		float aux1,aux2,resultado;
-		for(int i=0;i<operacion.size();i++){
-			string copia=operacion[i];
-			char copia2=copia[0];
-			if(isdigit(copia2)){	
-				pila->push(stoi(operacion[i]));
-				pila->print();
-			}
-			else{
-				operador=operacion[i];
-				aux1=pila->top();
-				pila->pop();
-				aux2=pila->top();
-				pila->pop();
-				if(operador=="+"){
-					resultado=aux1+aux2;
-					pila->push(resultado);
+			float aux1,aux2,resultado;
+			for(int i=0;i<operacion.size();i++){
+				string copia=operacion[i];
+				char copia2=copia[0];
+				if(isdigit(copia2)){	
+					pila->push(stoi(operacion[i]));
 					pila->print();
 				}
-				else
-					if(operador=="-"){
-						resultado=aux1-aux2;
+				else{
+					operador=operacion[i];
+					aux1=pila->top();
+					pila->pop();
+					aux2=pila->top();
+					pila->pop();
+					if(operador=="+"){
+						resultado=aux1+aux2;
 						pila->push(resultado);
 						pila->print();
 					}
-				else
-					if(operador=="/"){
-						resultado=aux1/aux2;
-						pila->push(resultado);
-						pila->print();
-					}
-				else
-					if(operador=="*"){
-						resultado=aux1*aux2;
-						pila->push(resultado);
-						pila->print();
-					}
+					else
+						if(operador=="-"){
+							resultado=aux1-aux2;
+							pila->push(resultado);
+							pila->print();
+						}
+					else
+						if(operador=="/"){
+							resultado=aux1/aux2;
+							pila->push(resultado);
+							pila->print();
+						}
+					else
+						if(operador=="*"){
+							resultado=aux1*aux2;
+							pila->push(resultado);
+							pila->print();
+						}
+			   }
 			}
-		}
-		}
+		}	
 }
 
 int menu(){
